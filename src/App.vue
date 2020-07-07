ae64e15c45563dc68627ec97616d6cf35b5130a5
shengxinjing <316783812@qq.com>
2020-07-08 00:51:23 +0800

    add demo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">foo</div>
+  <div class="app">foo</div>
+  <div class="app">foo</div>
+  <div class="app">foo</div>
+  <div class="app">foo</div>
+  <script>console.log(1)</script>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
